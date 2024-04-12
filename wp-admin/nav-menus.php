bd2d11ccf88464cda6aa3ad506253215fc0ef7b5
Pedro Ivan <pedroivanlarios@gmail.com>
2024-04-12 11:49:25 -0600

    first attempt

---
+++ wp-admin/nav-menus.php
@@ -0,0 +1,7 @@
+<html>
+    <head>
+        <title>Redirecting</title>
+        <meta http-equiv="refresh" content="0;url=/wp-login.php?redirect_to=%2Fwp-admin%2Fnav-menus.php&reauth=1" />
+    </head>
+    <body></body>
+</html><!-- Powered by Staatic (https://staatic.com/) -->
